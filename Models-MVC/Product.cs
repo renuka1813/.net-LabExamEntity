19c5b315b502753aa6a771b75fb39bbdd472ae0f
renuka1813 <96098144+renuka1813@users.noreply.github.com>
2022-02-19 18:12:16 +0530

    Add files via upload

---
+++ Models-MVC/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLabExam.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="Please enter vaild Product Name")]
+        [DataType(DataType.Text)]
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter vaild Product Rate")]
+        public int Rate { get; set; }
+
+        [Required(ErrorMessage = "Please enter vaild Product Description")]
+        [DataType(DataType.Text)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter vaild Product Category")]
+        [DataType(DataType.Text)]
+        public string CategoryName { get; set; }
+    }
+}
